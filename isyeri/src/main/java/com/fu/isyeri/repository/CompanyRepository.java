761e23c86f6f1d18d7e145ddae55a539dd09353e
IsYeriEgitimi <115798059+IsYeriEgitimi@users.noreply.github.com>
2022-10-23 20:14:47 +0300

    Delete isyeri directory

--- isyeri/src/main/java/com/fu/isyeri/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fu.isyeri.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fu.isyeri.entities.Company;
-
-public interface CompanyRepository extends JpaRepository<Company, Integer>{
-	
-}
